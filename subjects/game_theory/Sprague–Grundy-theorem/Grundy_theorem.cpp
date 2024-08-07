7b0d2aeffd3f5c31826e8552f9740cd4d086cabc
Rhavy Frota <rhavyf1411@gmail.com>
2024-08-06 22:26:41 -0300

    started game theory

---
+++ subjects/game_theory/Sprague–Grundy-theorem/Grundy_theorem.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+
+    Grundy generalizes the strategy from the game nim to every other game that fulfil the following
+
+    Two players move alternately. 
+    The game consists of states, and the possible moves in a state do not depend on whose turn it is. 
+    The game ends when a player cannot make a move. 
+    The game surely ends sooner or later. 
+    The players have complete information about the states and allowed moves, and there is no randomness in the game.
+
+    So, to deal with grundy numbers. We need to understand the following.
+
+    To calculate the grundy number of a number we need to do the following
+
+    g_s = mex({g1, g2,…, gn})
+
+    Where g1, g2, .., gn are the grundy numbers of the states to which we can move.
+    By the way mex({}) = 0.
+
+*/
+
+signed main()
+{
+
+    
+
+    return 0;
+}
